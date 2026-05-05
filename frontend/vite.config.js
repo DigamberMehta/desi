c0fa432892d60b6cf8c6cf42b759f174eedf64b2
Digamber Mehta <digambermehta2603@gmail.com>
2026-05-05 23:45:23 +0530

    a

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -31,12 +31,6 @@ export default defineConfig({
         },
       },
     },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-      },
-    },
+    minify: "esbuild",
   },
 });
